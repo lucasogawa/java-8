2610d0ec680597f6a49b244f7b164840d71d38d2
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-04-07 21:41:24 -0300

    Adding New Default Method ForEach

--- src/main/java/com/ogawalucas/java8/newdefaultmethodforeach/Java8.java
+++ src/main/java/com/ogawalucas/java8/newdefaultmethodforeach/Java8.java
@@ -1,12 +1,35 @@
 package com.ogawalucas.java8.newdefaultmethodforeach;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Java8 {
 
     public static void main(String[] args) {
-        List.of("word1", "word2", "word3")
-            .forEach(System.out::println);
+        var strings = new ArrayList<String>();
+
+        strings.add("string1");
+        strings.add("string333");
+        strings.add("string22");
+
+        strings.sort(new StringComparator());
+
+        strings.forEach(System.out::println);
     }
 }
 
+class StringComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String string1, String string2) {
+        if (string1.length() < string2.length()) {
+            return -1;
+        }
+
+        if (string1.length() == string2.length()) {
+            return 0;
+        }
+
+        return 1;
+    }
+}
